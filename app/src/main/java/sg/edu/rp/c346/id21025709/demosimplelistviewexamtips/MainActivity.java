6d60e4d8c34146ddfcb27b9383a9e3affbb8fa86
21025709-M <21025709@myrp.edu.sg>
2023-06-06 11:59:36 +0800

    Renamed items

--- app/src/main/java/sg/edu/rp/c346/id21025709/demosimplelistviewexamtips/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21025709/demosimplelistviewexamtips/MainActivity.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     ListView lvExamTips;
-    ArrayList<String> examTipsArray;
+    ArrayList<String> examTipsArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +19,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         this.setTitle("ArrayList");
 
-        examTipsArray = new ArrayList<String>();
-        examTipsArray.add("Don't just read the code, code it as much as possible during each practical session");
-        examTipsArray.add("Seek help from the lecturer ASAP, don't wait till you lost in the jungle");
-        examTipsArray.add("Prepare your template source code for each topic");
-        examTipsArray.add("Create a few empty Android projects to speed up your coding during the exam");
-        examTipsArray.add("Ensure that your Android Studio is up and running before the exam");
+        examTipsArrayList = new ArrayList<String>();
+        examTipsArrayList.add("Don't just read the code, code it as much as possible during each practical session");
+        examTipsArrayList.add("Seek help from the lecturer ASAP, don't wait till you lost in the jungle");
+        examTipsArrayList.add("Prepare your template source code for each topic");
+        examTipsArrayList.add("Create a few empty Android projects to speed up your coding during the exam");
+        examTipsArrayList.add("Ensure that your Android Studio is up and running before the exam");
 
         lvExamTips = findViewById(R.id.listViewExamTips);
 
         ArrayAdapter adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, examTipsArray);
+                android.R.layout.simple_list_item_1, examTipsArrayList);
 
         lvExamTips.setAdapter(adapter);
     }
